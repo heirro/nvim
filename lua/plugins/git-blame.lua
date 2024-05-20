eca8975233d42484ae2fe860132e395b8dabd62d
Vava Heirro <vavaheirro@live.com>
2024-05-20 14:18:02 +0700

    Intial commit

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "f-person/git-blame.nvim",
+    enabled = true,
+    config = function()
+      vim.g.gitblame_date_format = "%a %d %b %y %H:%M"
+    end,
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,87 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      servers = {
+        zls = {
+          mason = false,
+        },
+        gleam = {
+          mason = false,
+        },
+        gopls = {
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = false,
+                nilness = true,
+                unusedparams = true,
+                unusedvariable = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = false,
+              -- completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+        -- tsserver = {
+        -- 	settings = {
+        -- 		javascript = {
+        -- 			inlayHints = {
+        -- 				includeInlayEnumMemberValueHints = false,
+        -- 				includeInlayFunctionLikeReturnTypeHints = false,
+        -- 				includeInlayFunctionParameterTypeHints = true,
+        -- 				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+        -- 				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        -- 				includeInlayPropertyDeclarationTypeHints = false,
+        -- 				includeInlayVariableTypeHints = true,
+        -- 			},
+        -- 		},
+        -- 		typescript = {
+        -- 			inlayHints = {
+        -- 				includeInlayEnumMemberValueHints = false,
+        -- 				includeInlayFunctionLikeReturnTypeHints = false,
+        -- 				includeInlayFunctionParameterTypeHints = true,
+        -- 				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+        -- 				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        -- 				includeInlayPropertyDeclarationTypeHints = false,
+        -- 				includeInlayVariableTypeHints = true,
+        -- 			},
+        -- 		},
+        -- 	},
+        -- },
+      },
+    },
+  },
+}
